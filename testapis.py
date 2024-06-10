25c16c03aa6f856bbcd8b6230cd46eaa1a03a08d
Krishna595956 <kr4785543@gmail.com>
2024-06-10 10:21:33 +0530

    upate

---
+++ testapis.py
@@ -0,0 +1,51 @@
+# @app.route('/block0')
+# def block0():
+#     ledgers.insert_one({
+#         'blockid':0,
+#         'thash':'---',
+#         'dhash':'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+#         'thash':'---',
+#         'addedby':'---'
+#     })
+#     return 'added'
+
+# @app.route('/ledgertest')
+# # def ledtest():
+#     data = [
+#     {
+#         "blockid": "12345",
+#         "thash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",  # Replace with actual hash value
+#         "dhash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",  # Replace with actual hash value
+#         "phash": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",  # Replace with actual hash value
+#         "addedby": "user1"
+#     },
+#     {
+#         "blockid": "67890",
+#         "thash": "0x5678efgh...",  # Replace with actual hash value
+#         "dhash": "0xijklmnop...",  # Replace with actual hash value
+#         "phash": "0xcafebabe...",  # Replace with actual hash value
+#         "addedby": "user2"
+#     },
+#     {
+#         "blockid": "abcde",
+#         "thash": "0x90abcdef...",  # Replace with actual hash value
+#         "dhash": "0xqrstuvwx...",  # Replace with actual hash value
+#         "phash": "0xbadc0ded...",  # Replace with actual hash value
+#         "addedby": "user3"
+#     },
+#     {
+#         "blockid": "fghij",
+#         "thash": "0xdeadcafe...",  # Replace with actual hash value
+#         "dhash": "0vwxyz123...",  # Replace with actual hash value
+#         "phash": "0xfeedface...",  # Replace with actual hash value
+#         "addedby": "user4"
+#     },
+#     {
+#         "blockid": "jklmn",
+#         "thash": "0xbeefcafe...",  # Replace with actual hash value
+#         "dhash": "04567890...",  # Replace with actual hash value
+#         "phash": "0xdeadc0de...",  # Replace with actual hash value
+#         "addedby": "user5"
+#     }]
+#     return render_template('testledger.html',data=data)
+
